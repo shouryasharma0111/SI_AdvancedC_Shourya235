ece5d081fd78df139fc97f112e90d72c97c7128f
shouryasharma0111 <devpandit201009@gmail.com>
2026-06-23 07:55:38 +0530

    Work done on 22 June 2026.
    Updated the program to find the largest of three numbers.

--- Program13.c
+++ Program13.c
@@ -1,4 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 int main() {
-    int number;
+    double num1, num2, num3;
+    printf("Enter three numbers: ");
+    scanf("%lf %lf %lf", &num1, &num2, &num3);
+
+    if (num1 >= num2 && num1 >= num3) {
+        printf("%.2f is the largest number.\n", num1);
+    } 
+    else if (num2 >= num1 && num2 >= num3) {
+        printf("%.2f is the largest number.\n", num2);
+    } 
+    else {
+        printf("%.2f is the largest number.\n", num3);
+    }
+    return 0;
 }
